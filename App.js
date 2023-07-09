015ab56e5c9a98524182ed6ed2950d74c592a836
unknown <asankulov-t>
2023-07-09 23:10:24 +0600

    some changes

--- App.js
+++ App.js
@@ -1,49 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Button, FlatList, Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {req} from "./Api";
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState} from "react";
+
 
 export default function App() {
-  // const autoScroll = () => {
-  //   let offset = 0;
-  //   setInterval(()=> {
-  //     offset += 20
-  //     scrollViewRef.current?.scrollTo({x: 0, y: offset, animated: true})
-  //   }, 1000)
+  let CurrentSlide=0
+  let [active,setActive]=useState(0)
+  let refs=useRef()
+  let [offset,setOffset] = useState(20);
+  const [index, setIndex] = useState(0);
+
+  // const slowlyScrollDown = () => {
+  //   const y = offset + 80;
+  //   refs.current?.scrollTo({x: 0, y, animated: true});
+  //   setOffset(y);
   // }
+  const autoScroll = () => {
+    let offset = 0;
+    setInterval(()=> {
+      offset += 100
+      refs.current.scrollTo({x: 0, y: offset, animated: true})
+    }, 1000)
+  }
+  // setInterval(()=>{
+  //   if (offset >= data.length-1){
+  //     setOffset(0)
+  //   }else {
+  //     refs.current.scrollTo({
+  //       y:offset,
+  //       x:offset,
+  //       animated:true
+  //     })
+  //     console.log(refs.current.scrollTo)
+  //   }
+  // },6000)
+
+
   let [data,setData]=useState('')
-  let day=new Date().toLocaleDateString()
   useEffect(()=>{
-    req.getToken('08.07.2023').then(r=>setData(r))
-
+    req.getToken('10.07.2023').then(r=>setData(r))
+    autoScroll()
   },[])
-  const scrollViewRef = useRef();
-  console.log(data)
 
-  return (
-    <View style={styles.container}>
-      <ScrollView ref={scrollViewRef} >
-        {data&&data.map((t)=>{
-          return <View>
-            <Image source={{uri:'https://api.broadway.kg/files/'+t.picture.fileName+'.png'}} style={styles.img}/>
-            <Text>{t.filmName}</Text>
-          </View>
-        })}
-      </ScrollView>
-      <StatusBar style="auto" />
-    </View>
+  console.log(data)
+  return (<View style={styles.container}>
+        <ScrollView ref={refs}>
+          <Button onPress={autoScroll} title={'saasa'}/>
+          {data?data.map((t)=>{
+            return  <View style={styles.wrapper}>
+              <View>
+                <Image source={{uri:'https://api.broadway.kg/files/'+t.picture.fileName+'.png'}}
+                       style={styles.img}/>
+              </View>
+              <View style={styles.info}>
+                <Text style={styles.cur}>{CurrentSlide}</Text>
+                <Text style={styles.title}>{t.filmName}</Text>
+                <View><Text style={styles.sessionText}>Сеансы: </Text></View>
+                <View style={styles.sessions}>
+                  {t.times.map((t)=><Text key={t.time} style={styles.session}>{t.time}</Text>)}
+                </View>
+              </View>
+            </View>
+          }):<Text>hello</Text>}
+
+        </ScrollView>
+
+      </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#090909',
+
+  },
+  wrapper:{
+    display:'flex',
+    flexDirection:'row',
+    marginTop:25
+  },
+  cur:{
+    color:'#bee3ff'
+  },
+  info:{
+    marginLeft:25
+  },
+  title:{
+    fontSize:15,
+    color:'#fffcfa',
+    fontWeight:'bold',
+    marginTop: 8,
+
+  },
+    sessions:{
+      marginTop:15,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems:'center'
+    },
+  sessionText:{
+    color:'#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
+  session:{
+    textAlign:'center',
+    paddingRight:15,
+    paddingLeft:15,
+    color:'#000000',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginRight:30,
+    backgroundColor: '#fcc537',
+    borderWidth: 3,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius:10,
+    borderBottomRightRadius:10,
+    borderColor:'#f1f0f0'
   },
   img:{
-    width:'400px',
-    height:'250px'
+    width:50,
+    height:90,
   }
 });
---
+++ RenderItem.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import {FlatList, Text, View} from "react-native";
+//
+// const RenderItem = (props) => {
+//     return (
+//         <div>
+//             {data?<FlatList data={data}
+//                             ref={refs}
+//                             renderItem={(i)=><Item item={i}/>}
+//                             keyExtractor={item => item.filmName}
+//             />:<Text>'not found data'</Text
+//         </div>
+//     );
+// };
+//
+// export default RenderItem;
